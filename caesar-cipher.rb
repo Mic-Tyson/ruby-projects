ae0c58584b2ef0b5ef9ce46938b2ebd52b43543a
manraj <mkooner@live.com>
2024-05-03 19:17:34 +0100

    Add caesar-cypher for strings

---
+++ caesar-cipher.rb
@@ -0,0 +1,14 @@
+def caesar_cipher(string, shift_factor)
+    modified_chars = string.chars.map do |char|
+        case char
+        when 'a'..'z'
+            ((char.ord - 'a'.ord + shift_factor) % 26 + 'a'.ord).chr 
+        when 'A'..'Z'
+            ((char.ord - 'A'.ord + shift_factor) % 26 + 'A'.ord).chr 
+        else char
+        end
+    end
+    modified_chars.join    
+end 
+
+p caesar_cipher("Hello Odin!", 5)
