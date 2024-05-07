8cb02c1d8b1dc0d14eabe15d02f4ffe962d4b6a4
Mic-Tyson <mkooner@live.com>
2024-05-07 10:58:07 +0100

    Refactor code according to Ruby standards

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,4 @@
-# to create the code, the class will have access to a hat of 
+# to create the code, the class will have access to a hat of
 # potential options
 # from that hat we will keep on picking random elements, while using uniq to ensure
 # that we haven't reselected an option
@@ -7,14 +7,12 @@
 # elem in the code
 # we then use symbols to display exact, partial, and full matches
 
-
 class Game
   # handle io, and mastermind
-  
-  def initialize()
+
+  def initialize
     @mastermind = Mastermind.new
   end
-
 
   def play
     puts "Guess my code using the following colors #{@mastermind.code_maker.CODE_OPTIONS}"
@@ -22,43 +20,38 @@ class Game
       guess = get_guess
       feedback = @mastermind.compare_guess(guess)
       p feedback
-      return p "you win" if feedback.all?('O')
+      return p 'you win' if feedback.all?('O')
     end
     "You lose! The correct code was: #{@mastermind.code}"
-  end 
+  end
 
+  private
 
-  private 
-  
   def validate_guess?(guess)
     guess.size == @mastermind.code_maker.CODE_LENGTH &&
-    guess.all? {|elem| @mastermind.code_maker.CODE_OPTIONS.include?(elem)}
+      guess.all? { |elem| @mastermind.code_maker.CODE_OPTIONS.include?(elem) }
   end
 
   def get_guess
     # guesses will be of the form g1, g2, g3
     guess = ''
-    puts "Enter your guess"
-    loop do 
+    puts 'Enter your guess'
+    loop do
       guess = gets.chomp.downcase
       guess = guess.split(', ')
       return guess if validate_guess?(guess)
+
       puts "Invalid guess. Please enter #{@mastermind.code_maker.CODE_LENGTH} comma-separated colours."
     end
   end
-
-
 end
 
-
 # red, yellow, orange, white, green
-
-
 
 class Mastermind
   attr_reader :code, :code_maker
 
-  def initialize()
+  def initialize
     @code_maker = Code_maker.new
     @code = @code_maker.make_code
   end
@@ -70,41 +63,38 @@ class Mastermind
   end
 
   private
-  
+
   def compare_exact(guess, code)
     code.each_with_index.reduce([]) do |a, (elem, i)|
       guess[i] == elem ? a.push('O') : a.push('X')
     end
   end
-  
+
+  # algorithm taken from my bubble_sort, so could be refactored if I modify my sort to accept predicates
   def compare_partial(a, guess, code)
-    # algorithm taken from my bubble_sort, so could be refactored if I modify my sort to accept predicates
-    code.each_with_index.reduce(a) do |a, (elem, i)| 
+    code.each_with_index.reduce(a) do |a, (_, i)|
       unless a[i] == 'O'
         j = 0
         while j < code.length
-            if guess[i] == code[j]
-              a[i] = '?'
-            end
-            j+=1
+          if guess[i] == code[j]
+            a[i] = '?'
           end
+          j += 1
         end
-          a
+      end
+      a
     end
   end
-
-  
 end
 
 class Code_maker
-
-  @@CODE_OPTIONS = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white"]
+  @@CODE_OPTIONS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
   @@CODE_LENGTH = 5
 
   def make_code
     code = @@CODE_OPTIONS.sample(@@CODE_LENGTH).uniq # ideally get 5 random elems
     code += @@CODE_OPTIONS.sample(@@CODE_LENGTH - code.size) until code.size == @@CODE_LENGTH # keep adding samples
-    return code
+    code
   end
 
   def CODE_OPTIONS
@@ -114,12 +104,8 @@ class Code_maker
   def CODE_LENGTH
     @@CODE_LENGTH
   end
-
 end
 
-
-test = Game.new()
-guess = ["red", "white", "blue", "orange", "yellow"]
-
+test = Game.new
 
 test.play
