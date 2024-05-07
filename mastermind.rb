a094b99765679e081779c495999faf62f3dd063e
Mic-Tyson <mkooner@live.com>
2024-05-07 13:19:06 +0100

    Add code for player to create code

--- mastermind.rb
+++ mastermind.rb
@@ -7,9 +7,8 @@
 # elem in the code
 # we then use symbols to display exact, partial, and full matches
 
+# handle io, and mastermind
 class Game
-  # handle io, and mastermind
-
   def initialize
     @mastermind = Mastermind.new
   end
@@ -18,11 +17,24 @@ class Game
     puts "Guess my code using the following colors #{@mastermind.code_maker.CODE_OPTIONS}"
     12.times do
       guess = get_guess
+      puts 'Enter your guess'
       feedback = @mastermind.compare_guess(guess)
       p feedback
       return p 'you win' if feedback.all?('O')
     end
     "You lose! The correct code was: #{@mastermind.code}"
+  end
+
+  def get_code
+    puts 'Enter a 5 digit, comma-seperated, code utilising the following colours:'
+    p @mastermind.code_maker.CODE_OPTIONS
+
+    loop do
+      code = get_guess
+      return code if code.uniq.length == 5
+
+      puts 'You aren\'t allowed to reuse colours'
+    end
   end
 
   private
@@ -33,21 +45,18 @@ class Game
   end
 
   def get_guess
-    # guesses will be of the form g1, g2, g3
     guess = ''
-    puts 'Enter your guess'
     loop do
       guess = gets.chomp.downcase
       guess = guess.split(', ')
       return guess if validate_guess?(guess)
 
-      puts "Invalid guess. Please enter #{@mastermind.code_maker.CODE_LENGTH} comma-separated colours."
+      puts "Invalid input. Please enter #{@mastermind.code_maker.CODE_LENGTH} comma-separated colours."
     end
   end
 end
 
-# red, yellow, orange, white, green
-
+# handle code checking and creation
 class Mastermind
   attr_reader :code, :code_maker
 
@@ -71,22 +80,22 @@ class Mastermind
   end
 
   # algorithm taken from my bubble_sort, so could be refactored if I modify my sort to accept predicates
-  def compare_partial(a, guess, code)
-    code.each_with_index.reduce(a) do |a, (_, i)|
-      unless a[i] == 'O'
+  def compare_partial(arr, guess, code)
+    code.each_with_index.reduce(arr) do |accumulator, (_, i)|
+      unless accumulator[i] == 'O'
         j = 0
         while j < code.length
-          if guess[i] == code[j]
-            a[i] = '?'
-          end
+          accumulator[i] = '?' if guess[i] == code[j]
+
           j += 1
         end
       end
-      a
+      accumulator
     end
   end
 end
 
+# handle code validation and cpu generation
 class Code_maker
   @@CODE_OPTIONS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
   @@CODE_LENGTH = 5
@@ -108,4 +117,6 @@ end
 
 test = Game.new
 
-test.play
+p test.get_code
+
+# red, yellow, orange, white, green
